b95c9e291427257f1a890b4479fd3ef4997b76cb
enricBc <enricbeltrancano@gmail.com>
2018-06-20 16:01:59 +0200

    Stylish

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,8 +11,14 @@
   </head>
     <body>
       <div class="container">
-        <h1>StatusWebPageCaptio</h1>
-        <div class="row">
+        <div class="col-12">
+          <div class="row">
+            <h1 style="color:#2899c1;">StatusWebPage</h1>
+            <h1 style="color:#f9ba14">Captio</h1>
+          </div>
+          <hr style="color:#666;">
+        </div>
+        <div class="row ml-2">
           <div class="col-12">
             <div class="row mt-3 mb-5">
               <h1 class="col-12" style="color:#2899c1;">Tickets <i class="fas fa-file-image" style="color:#f9ba14"></i></h1>
